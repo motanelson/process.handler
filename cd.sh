c85007d36c6fa56f73746c899b307be41b37847e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-06-05 08:54:13 +0100

    Add files via upload

---
+++ cd.sh
@@ -0,0 +1,30 @@
+mkdir -p CD_root/isolinux
+mkdir -p CD_root/images
+mkdir -p CD_root/kernel
+cp isolinux.bin CD_root/isolinux/isolinux.bin
+cp isolinux.cfg CD_root/isolinux/isolinux.cfg
+
+cp *.bss CD_root/isolinux/
+cp *.sys CD_root/isolinux/
+cp *.c32 CD_root/isolinux/
+cp *.com CD_root/isolinux/
+cp *.elf CD_root/isolinux/
+cp *.jpg CD_root/isolinux/
+cp *.menu CD_root/isolinux/
+cp *.txt CD_root/isolinux/
+ls CD_root/isolinux > CD_root/isolinux/ls.txt
+bcc -c -Md libdos.c -o libdos.a
+bcc -x -i -L -Md intro.c -o intro.com
+bcc -x -i -L -Md cat.c -o cat.com
+bcc -x -i -L -Md ls.c -o ls.com
+bcc -x -i -L -Md echo.c -o echo.com
+bcc -x -i -L -Md printf.c -o printf.com
+bcc -x -i -L -Md reboot.c -o reboot.com
+bcc -x -i -L -Md pwd.c -o pwd.com
+bcc -x -i -L -Md clear.c -o clear.com
+bcc -x -i -L -Md sleep.c -o sleep.com
+
+cp *.c32 CD_root/isolinux/
+cp *.com CD_root/isolinux/
+genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
+
